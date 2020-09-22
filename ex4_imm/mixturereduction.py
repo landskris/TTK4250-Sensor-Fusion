99bd1f2966ca0c5f5d8fc59e8976056b5ab44134
landskris <kr-lands@online.no>
2020-09-22 15:11:01 +0200

    Added gaussian, to test

--- ex4_imm/mixturereduction.py
+++ ex4_imm/mixturereduction.py
@@ -13,17 +13,18 @@ def gaussian_mixture_moments(
     """Calculate the first two moments of a Gaussian mixture"""
 
     # mean
-    mean_bar = None  # TODO: hint np.average using axis and weights argument
+    mean_bar = np.average(mean, axis=0, weights=w)  # TODO: hint np.average using axis and weights argument
 
     # covariance
     # # internal covariance
-    cov_int = None  # TODO: hint, also an average
+    cov_int = np.average(cov, axis=0, weights=w)  # TODO: hint, also an average
 
     # # spread of means
-    # Optional calc: mean_diff =
-    cov_ext = None  # TODO: hint, also an average
+    sum_diff = [w * my @ my.T for w, my in zip(w, mean)]
+    mean_diff = np.sum(sum_diff, axis=0)
+    cov_ext = mean_diff - mean_bar @ mean_bar.T  # TODO: hint, also an average
 
     # # total covariance
-    cov_bar = None  # TODO
+    cov_bar = cov_int + cov_ext
 
     return mean_bar, cov_bar
